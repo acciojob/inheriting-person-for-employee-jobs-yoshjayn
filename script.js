57a0065f23bdc0fefea50bbe3ea30b53ca2f1781
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-03-02 00:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// complete this js code
-function Person(name, age) {}
-
-function Employee(name, age, jobTitle) {}
-
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+class Person{
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+    greet(){
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+    }
+}
+class Employee extends Person{
+    constructor(name,age,jobTitle){
+        super(name,age)
+        this.jobTitle = jobTitle
+    }
+    jobGreet(){
+        
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+    }
+}
