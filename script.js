2ab9869719cf682f1e29af61229c90b4f5e0237a
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-03-02 10:32:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-class Person{
-    constructor(name, age){
-        this.name = name
-        this.age = age
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    greet(){
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-    }
-}
-class Employee extends Person{
-    constructor(name,age,jobTitle){
-        super(name,age)
-        this.jobTitle = jobTitle
-    }
-    jobGreet(){
-        
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 }
+
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+}
+
+// Attach to window so Cypress can access them
+window.Person = Person;
+window.Employee = Employee;
